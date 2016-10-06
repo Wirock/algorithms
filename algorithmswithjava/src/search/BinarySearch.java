683cfdea60d925af92ed8efdc1181c87c5cfab92
Wirock <154108697@qq.com>
2016-10-06 23:46:55 +0800

    二分查找

--- algorithmswithjava/src/search/BinarySearch.java
+++ algorithmswithjava/src/search/BinarySearch.java
@@ -1,15 +1,41 @@
 package search;
-
+/**
+ * 二分查询
+ * @author Administrator
+ *
+ */
 public class BinarySearch {
-	
-//	public static int binarySearch(int[] array){
-//		
-//	}
+	/**
+	 * 二分查找
+	 * @param array进行查找的有序数组
+	 * @param from查找起始坐标
+	 * @param to查找终止坐标
+	 * @param key查找的数
+	 * @return key在array中的坐标
+	 */
+	public static int binarySearch(int[] array,int from,int to,int key){
+		if(from<0||to<0){
+			throw new IllegalArgumentException("参数开始坐标和数组长度必须大于0");
+		}
+		if(from<=to){
+			int middle = (from+to)/2;
+			int temp = array[middle];
+			if(temp>key){
+				to = middle-1;
+			}else if(temp<key){
+				from = middle + 1;
+			}else{
+				return middle;
+			}
+		}
+		return binarySearch(array,from,to,key);
+	}
 
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] array = {0,1,2,3,4,5,6,7,8,9};
+		int i = binarySearch(array,0,array.length-1,5);
+		System.out.println(i);
 	}
 
 }
