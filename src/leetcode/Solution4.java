e3870ed22fb3931b585560d7471546e3fa1742f0
Chenzw <154108697@qq.com>
2021-03-08 00:15:03 +0800

    leetCode

--- src/leetcode/Solution4.java
+++ src/leetcode/Solution4.java
@@ -40,30 +40,30 @@ public class Solution4 {
     public static double findMedianSortedArrays2(int[] nums1, int[] nums2) {
         int n = nums1.length;
         int m = nums2.length;
-        int left = (n + m + 1) / 2;
-        int right = (n + m + 2) / 2;
+        int left = (n + m -1) / 2;
+        int right = (n + m) / 2;
         //将偶数和奇数的情况合并，如果是奇数，会求两次同样的 k 。
-        return (findKthElement(nums1, 0, n - 1, nums2, 0, m - 1, left) + findKthElement(nums1, 0, n - 1, nums2, 0, m - 1, right))/2.0;
+        return (findKthElement(nums1, 0, nums2, 0, left) + findKthElement(nums1, 0, nums2, 0, right))/2.0;
     }
 
 
-    private static int findKthElement(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
-        int len1 = end1 - start1 + 1;
-        int len2 = end2 - start2 + 1;
+    private static int findKthElement(int[] nums1, int start1, int[] nums2, int start2, int k) {
+        int len1 = nums1.length - start1 ;
+        int len2 = nums2.length - start2 ;
         //让 len1 的长度小于 len2，这样就能保证如果有数组空了，一定是 len1
-        if (len1 > len2) return findKthElement(nums2, start2, end2, nums1, start1, end1, k);
-        if (len1 == 0) return nums2[start2 + k - 1];
+        if (len1 > len2) return findKthElement(nums2, start2, nums1, start1, k);
+        if (len1 == 0) return nums2[start2 + k ];
 
-        if (k == 1) return Math.min(nums1[start1], nums2[start2]);
+        if (k == 0) return Math.min(nums1[start1], nums2[start2]);
 
-        int i = start1 + Math.min(len1, k / 2) - 1;
-        int j = start2 + Math.min(len2, k / 2) - 1;
+        int i = start1 + Math.min(len1-1, (k-1) / 2);
+        int j = start2 + Math.min(len2-1, (k-1) / 2);
 
         if (nums1[i] > nums2[j]) {
-            return findKthElement(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1));
+            return findKthElement(nums1, start1, nums2, j + 1,  k - (j - start2 + 1));
         }
         else {
-            return findKthElement(nums1, i + 1, end1, nums2, start2, end2, k - (i - start1 + 1));
+            return findKthElement(nums1, i + 1,  nums2, start2, k - (i - start1 + 1));
         }
     }
     public static void main(String[] args) {
