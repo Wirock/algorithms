8539c99be0bced8670ff0d135850715c1c2ec625
Chenzw <154108697@qq.com>
2021-03-10 23:44:02 +0800

    leetCode

---
+++ src/leetcode/Solution7.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+/**
+ * 7. 整数反转
+ * 给你一个 32 位的有符号整数 x ，返回 x 中每位上的数字反转后的结果。
+ *
+ * 如果反转后整数超过 32 位的有符号整数的范围 [−231,  231 − 1] ，就返回 0。
+ *
+ * 假设环境不允许存储 64 位整数（有符号或无符号）。
+ * @author chenzw
+ * @date 2021/3/10
+ */
+public class Solution7 {
+    public static int reverse(int x) {
+        int rev = 0;
+        while(x!=0){
+            int remain = x%10;
+            if(rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && remain > 7))     return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && remain < -8))return 0;
+            rev = rev*10+remain;
+            x = x/10;
+        }
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse(123));
+        System.out.println(reverse(-123));
+    }
+}
