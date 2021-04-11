9b05e60927c1873d40c4f1e52d1bfcc5e43d3407
Chenzw <154108697@qq.com>
2021-04-11 19:42:02 +0800

    leetCode264 solved by math

---
+++ src/leetcode/Solution264.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+/**
+ * 264. 丑数 II
+ * 给你一个整数 n ，请你找出并返回第 n 个 丑数 。
+ *
+ * 丑数 就是只包含质因数 2、3 和/或 5 的正整数。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：n = 10
+ * 输出：12
+ * 解释：[1, 2, 3, 4, 5, 6, 8, 9, 10, 12] 是由前 10 个丑数组成的序列。
+ * 示例 2：
+ *
+ * 输入：n = 1
+ * 输出：1
+ * 解释：1 通常被视为丑数。
+ * @author chenzw
+ * @date 2021/4/10
+ */
+public class Solution264 {
+    public static int nthUglyNumber(int n) {
+        // ans 用作存储已有丑数（从下标 1 开始存储，第一个丑数为 1）
+        int[] ans = new int[n + 1];
+        ans[1] = 1;
+        // 由于三个有序序列都是由「已有丑数」*「质因数」而来
+        // i2、i3 和 i5 分别代表三个有序序列当前使用到哪一位「已有丑数」下标（起始都指向 1）
+        for (int i2 = 1, i3 = 1, i5 = 1, idx = 2; idx <= n; idx++) {
+            // 由 ans[iX] * X 可得当前有序序列指向哪一位
+            int a = ans[i2] * 2, b = ans[i3] * 3, c = ans[i5] * 5;
+            // 将三个有序序列中的最小一位存入「已有丑数」序列，并将其下标后移
+            int min = Math.min(a, Math.min(b, c));
+            // 由于可能不同有序序列之间产生相同丑数，因此只要一样的丑数就跳过（不能使用 else if ）
+            if (min == a) i2++;
+            if (min == b) i3++;
+            if (min == c) i5++;
+            ans[idx] = min;
+        }
+        return ans[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(nthUglyNumber(1690));
+    }
+}
