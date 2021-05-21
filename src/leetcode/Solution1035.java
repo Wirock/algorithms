3dc1c0607c5e3260995d6d09aa03127298b1b5b4
Chenzw <154108697@qq.com>
2021-05-21 09:44:43 +0800

    leetCode 1035 solved by dynamic programming

--- src/leetcode/Solution1035.java
+++ src/leetcode/Solution1035.java
@@ -43,12 +43,6 @@ public class Solution1035 {
 
     public static int maxUncrossedLines(int[] nums1, int[] nums2) {
         int[][] dp = new int[nums1.length+1][nums2.length+1];
-        for(int i=0;i<dp.length;i++){
-            dp[i][0]=0;
-        }
-        for(int i=1;i<dp[0].length;i++){
-            dp[0][i]=0;
-        }
         for(int i=1;i<dp.length;i++){
             for(int j=1;j<dp[0].length;j++){
                 dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
