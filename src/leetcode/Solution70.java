43bc2f7033c5cf6603ea8689e30722cd7aac9211
Chenzw <154108697@qq.com>
2021-05-24 14:24:46 +0800

    leetCode 70 solved by dp

--- src/leetcode/Solution70.java
+++ src/leetcode/Solution70.java
@@ -1,19 +1,39 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 /**
- * 90. 子集 II
- * 给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的子集（幂集）。
+ * 70. 爬楼梯
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
  *
- * 解集 不能 包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
  *
+ * 注意：给定 n 是一个正整数。
  *
+ * 示例 1：
+ *
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ * 示例 2：
+ *
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
  * @author chenzw
  * @date 2021/3/31
  */
 public class Solution70 {
-
+    public int climbStairs(int n) {
+        int[] dp = new int[2];
+        dp[0]=1;
+        dp[1]=2;
+        for(int i=2;i<n;i++){
+            dp[i%2]=dp[0]+dp[1];
+        }
+        return dp[(n-1)%2];
+    }
 }
