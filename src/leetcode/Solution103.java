80f19c48d857216e4c2899869b8a69013841313d
Chenzw <154108697@qq.com>
2021-06-21 14:42:57 +0800

    leetCode 102,103 solved by bfs

--- src/leetcode/Solution103.java
+++ src/leetcode/Solution103.java
@@ -7,48 +7,48 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * 102. 二叉树的层序遍历
- * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+ * 103. 二叉树的锯齿形层序遍历
+ * 给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
  *
- *
- *
- * 示例：
- * 二叉树：[3,9,20,null,null,15,7],
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
  *
  *     3
  *    / \
  *   9  20
  *     /  \
  *    15   7
- * 返回其层序遍历结果：
+ * 返回锯齿形层序遍历如下：
  *
  * [
  *   [3],
- *   [9,20],
+ *   [20,9],
  *   [15,7]
  * ]
  * @author chenzw
  * @date 2021/6/21
  */
-public class Solution102 {
-    public List<List<Integer>> levelOrder(TreeNode root) {
+public class Solution103 {
+    public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
         if(root==null)return ans;
         LinkedList<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
+        boolean reverse = false;
         while(!queue.isEmpty()){
             int size = queue.size();
             List<Integer> layer = new ArrayList<>(size);
             while(size>0){
                 root = queue.poll();
-                layer.add(root.val);
+                if(reverse)layer.add(0,root.val);
+                else layer.add(root.val);
                 if(root.left!=null)queue.offer(root.left);
                 if(root.right!=null)queue.offer(root.right);
                 size--;
             }
+            reverse = !reverse;
             ans.add(layer);
         }
         return ans;
     }
-
 }
