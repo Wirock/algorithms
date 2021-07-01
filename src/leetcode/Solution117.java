73a7c3fce7b1b49b5a621a29504e439c701be7f4
Chenzw <154108697@qq.com>
2021-07-02 01:04:08 +0800

    leetcode 117 solved by linkedList and bfs

---
+++ src/leetcode/Solution117.java
@@ -0,0 +1,86 @@
+package leetcode;
+
+import common.CommonUtil;
+import datastructure.tree.Node;
+
+/**
+ * 117. 填充每个节点的下一个右侧节点指针 II
+ * 给定一个二叉树
+ *
+ * struct Node {
+ *   int val;
+ *   Node *left;
+ *   Node *right;
+ *   Node *next;
+ * }
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ *
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ *
+ *
+ *
+ * 进阶：
+ *
+ * 你只能使用常量级额外空间。
+ * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+ *
+ *
+ * 示例：
+ *
+ *
+ *
+ * 输入：root = [1,2,3,4,5,null,7]
+ * 输出：[1,#,2,3,#,4,5,7,#]
+ * 解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。序列化输出按层序遍历顺序（由 next 指针连接），'#' 表示每层的末尾。
+ *
+ *
+ * 提示：
+ *
+ * 树中的节点数小于 6000
+ * -100 <= node.val <= 100
+ * @author chenzw
+ * @date 2021/6/23
+ */
+public class Solution117 {
+    //用链表来进行bfs
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Node cur = root;
+        Node pre = null;
+        Node start = null;
+        while (cur != null) {
+            if(cur.left!=null){
+                if(start==null)start=cur.left;
+                if(pre==null){
+                    pre=cur.left;
+                }else{
+                    pre.next=cur.left;
+                    pre = pre.next;
+                }
+            }
+            if(cur.right!=null){
+                if(start==null)start=cur.right;
+                if(pre==null){
+                    pre=cur.right;
+                }else{
+                    pre.next=cur.right;
+                    pre = pre.next;
+                }
+            }
+            if(cur.next!=null) {
+                cur = cur.next;
+            }else {
+                cur = start;
+                start = null;
+                pre = null;
+            }
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        CommonUtil.printTree(CommonUtil.arrays2BSTByLevel(new Integer[]{-9,-3,2,null,4,4,0,-6,null,-5}));
+    }
+}
