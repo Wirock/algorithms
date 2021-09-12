016ec03a542036a2c057fcd7784a67e34f9049d0
Chenzw <154108697@qq.com>
2021-09-13 00:45:19 +0800

    leetCode 678 solved by greedy algorithm

---
+++ src/leetcode/Solution678.java
@@ -0,0 +1,52 @@
+package leetcode;
+
+/**
+ * 678. 有效的括号字符串
+ * 给定一个只包含三种字符的字符串：（ ，） 和 *，写一个函数来检验这个字符串是否为有效字符串。有效字符串具有如下规则：
+ *
+ * 任何左括号 ( 必须有相应的右括号 )。
+ * 任何右括号 ) 必须有相应的左括号 ( 。
+ * 左括号 ( 必须在对应的右括号之前 )。
+ * * 可以被视为单个右括号 ) ，或单个左括号 ( ，或一个空字符串。
+ * 一个空字符串也被视为有效字符串。
+ * 示例 1:
+ *
+ * 输入: "()"
+ * 输出: True
+ * 示例 2:
+ *
+ * 输入: "(*)"
+ * 输出: True
+ * 示例 3:
+ *
+ * 输入: "(*))"
+ * 输出: True
+ * 注意:
+ *
+ * 字符串大小将在 [1，100] 范围内。
+ * @author chenzw
+ * @date 2021/9/13
+ */
+public class Solution678 {
+    public boolean checkValidString(String s) {
+        int minCount = 0;
+        int maxCount = 0;
+        char[] cs = s.toCharArray();
+        for(char c:cs){
+            if(c == '('){
+                maxCount++;
+                minCount++;
+            }
+            else if(c == ')'){
+                maxCount--;
+                minCount = Math.max(minCount - 1, 0);
+            }
+            else if(c == '*'){
+                maxCount++;
+                minCount = Math.max(minCount - 1, 0);
+            }
+            if(maxCount<0)return false;
+        }
+        return minCount<=0;
+    }
+}
