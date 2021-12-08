dfaf47dbbd2029b09ae04dff4fdf8a93631b028e
Chenzw <154108697@qq.com>
2021-12-08 23:59:05 +0800

    leetCode 689 solved by dp

---
+++ src/leetcode/Solution689.java
@@ -0,0 +1,61 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 689. 三个无重叠子数组的最大和
+ * 给你一个整数数组 nums 和一个整数 k ，找出三个长度为 k 、互不重叠、且 3 * k 项的和最大的子数组，并返回这三个子数组。
+ *
+ * 以下标的数组形式返回结果，数组中的每一项分别指示每个子数组的起始位置（下标从 0 开始）。如果有多个结果，返回字典序最小的一个。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,2,1,2,6,7,5,1], k = 2
+ * 输出：[0,3,5]
+ * 解释：子数组 [1, 2], [2, 6], [7, 5] 对应的起始下标为 [0, 3, 5]。
+ * 也可以取 [2, 1], 但是结果 [1, 3, 5] 在字典序上更大。
+ * 示例 2：
+ *
+ * 输入：nums = [1,2,1,2,1,2,1,2,1], k = 2
+ * 输出：[0,2,4]
+ *
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 2 * 104
+ * 1 <= nums[i] < 216
+ * 1 <= k <= floor(nums.length / 3)
+ * @author chenzw
+ * @date 2021/12/8
+ */
+public class Solution689 {
+    //前缀和+dp
+    public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
+        int n = nums.length;
+        long[] sum = new long[n + 1];
+        for (int i = 1; i <= n; i++) sum[i] = sum[i - 1] + nums[i - 1];
+        long[][] f = new long[n + 10][4];
+        for (int i = n - k + 1; i >= 1; i--) {
+            for (int j = 1; j < 4; j++) {
+                f[i][j] = Math.max(f[i + 1][j], f[i + k][j - 1] + sum[i + k - 1] - sum[i - 1]);
+            }
+        }
+        int[] ans = new int[3];
+        int i = 1, j = 3, idx = 0;
+        while (j > 0) {
+            if (f[i + 1][j] > f[i + k][j - 1] + sum[i + k - 1] - sum[i - 1]) {
+                i++;
+            } else {
+                ans[idx++] = i - 1;
+                i += k; j--;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Solution689().maxSumOfThreeSubarrays(new int[]{1,2,1,2,6,7,5,1},2)));
+    }
+}
