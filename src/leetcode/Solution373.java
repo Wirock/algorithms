625c8def449d3eb7f420f673fb019d0d06d038d4
Chenzw <154108697@qq.com>
2022-01-14 23:27:27 +0800

    leetCode 373 solved by priority queue

---
+++ src/leetcode/Solution373.java
@@ -0,0 +1,69 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ * 373. 查找和最小的 K 对数字
+ * 给定两个以 升序排列 的整数数组 nums1 和 nums2 , 以及一个整数 k 。
+ *
+ * 定义一对值 (u,v)，其中第一个元素来自 nums1，第二个元素来自 nums2 。
+ *
+ * 请找到和最小的 k 个数对 (u1,v1),  (u2,v2)  ...  (uk,vk) 。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+ * 输出: [1,2],[1,4],[1,6]
+ * 解释: 返回序列中的前 3 对数：
+ *      [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+ * 示例 2:
+ *
+ * 输入: nums1 = [1,1,2], nums2 = [1,2,3], k = 2
+ * 输出: [1,1],[1,1]
+ * 解释: 返回序列中的前 2 对数：
+ *      [1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
+ * 示例 3:
+ *
+ * 输入: nums1 = [1,2], nums2 = [3], k = 3
+ * 输出: [1,3],[2,3]
+ * 解释: 也可能序列中所有的数对都被返回:[1,3],[2,3]
+ *
+ *
+ * 提示:
+ *
+ * 1 <= nums1.length, nums2.length <= 105
+ * -109 <= nums1[i], nums2[i] <= 109
+ * nums1 和 nums2 均为升序排列
+ * 1 <= k <= 1000
+ * @author chenzw
+ * @date 2022/1/14
+ */
+public class Solution373 {
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        PriorityQueue<List<Integer>> pq = new PriorityQueue<>((x, y)->y.get(0)+y.get(1)-x.get(0)-x.get(1));
+        for(int i=0;i<Math.min(k,nums1.length);i++){
+            for(int j=0;j<Math.min(k,nums2.length);j++){
+                if(pq.size()==k){
+                    List<Integer> peek = pq.peek();
+                    if(peek.get(0)+peek.get(1)>nums1[i]+nums2[j]){
+                        pq.poll();
+                        List<Integer> list = new ArrayList<>(2);
+                        list.add(nums1[i]);
+                        list.add(nums2[j]);
+                        pq.offer(list);
+                    }
+                }else{
+                    List<Integer> list = new ArrayList<>(2);
+                    list.add(nums1[i]);
+                    list.add(nums2[j]);
+                    pq.offer(list);
+                }
+            }
+        }
+        return new ArrayList<>(pq);
+    }
+}
